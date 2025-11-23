57c7394a4a49520324fe8e3bbddf32cbed657dad
tyguy <jtyler0226@gmail.com>
2025-11-23 10:35:06 -0600

    added repl functionality

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan() 
+		line := scanner.Text()
+		clean := cleanInput(line)
+		fmt.Printf("Your command was: %s\n", clean[0])
+	}
 }
